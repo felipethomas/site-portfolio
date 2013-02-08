39562ebce6ade752ecbc193f504f798773c55d52
insystem <felipecejug@gmail.com>
2013-02-08 11:59:54 -0300

    renomeacao de arquivos para extensao php

---
+++ work.php
@@ -0,0 +1,187 @@
+	<?php require_once('header.php'); ?>
+		
+	<div class="hero-unit">
+		<div class="ac">
+			<h1>Meet the best of our work!</h1>
+			<p>This is a template for a simple portfolio page with tags.</p>
+		</div>
+	</div>
+
+	<div id="portfolio">
+
+		<!-- portfolio filter -->		
+		<ul id="portfolio-filter" class="nav nav-tabs">
+			<li class="active"><a href="#" class="filter" data-filter="*">All</a></li>
+			<li><a href="#" class="filter" data-filter=".web">Web</a></li>
+			<li><a href="#" class="filter" data-filter=".design">Design</a></li>
+			<li><a href="#" class="filter" data-filter=".corporate">Corporate identity</a></li>
+			<li><a href="#" class="filter" data-filter=".mobile">Mobile app</a></li>
+		</ul>
+
+		<!-- portfolio items -->		
+		<div class="row" id="portfolio-items">
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="design">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/1.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="corporate">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/2.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="mobile">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/3.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="web">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/4.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="mobile">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/5.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->												
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="corporate">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/6.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="design">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/7.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->
+
+			<!-- portfolio item-->
+			<div class="span4 project zoom" data-tags="mobile">
+
+				<a class="thumbnail" href="project.html">
+					<!-- image -->
+					<img src="assets/8.gif" alt="" />
+					<!-- name -->
+					<b class="project-name">Title</b>
+				</a>
+			</div>
+			<!-- end portfolio item-->						
+		</div>
+
+	</div>	</div>
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="span4">
+					<h3>YourSite</h3>
+					<ul>
+						<li><a href="#" title="">Home</a></li>
+						<li><a href="#" title="">Our work</a></li>
+						<li><a href="#" title="">About us</a></li>
+						<li><a href="#" title="">Pricing</a></li>
+						<li><a href="#" title="">Explore</a></li>
+						<li><a href="#" title="">Contact</a></li>
+					</ul>
+				</div>
+				<div class="span4 social-networks">
+					<h3>Stay in touch</h3>
+					<p>Stay in touch on social networks</p>
+					<a href="#" title="Follow us on Facebook" class="icon-facebook"></a>
+					<a href="#" title="Follow us on Twitter" class="icon-twitter"></a>
+					<a href="#" title="Follow us on Google Plus" class="icon-google-plus"></a>
+					<a href="#" title="Follow us on Google Plus" class="icon-github"></a>
+					<a href="#" title="Follow us on Google Plus" class="icon-pinterest"></a>
+				</div>
+				<div class="span4">
+					<h3>Newsletter</h3>
+					<p>Subscribe to our monthly newsletter and be the first to know about our news and special deals!</p>
+					<form>
+						<input type="text" placeholder="Enter your E-mail" />
+						<input type="submit" class="btn" value="Ok" />
+					</form>
+				</div>
+			</div> <!-- end .row -->
+		</div> <!-- end .container -->
+
+		<div id="footer-extra">
+			<div class="container">
+				<div class="row">
+					<div class="span4">
+						&copy; 2010—2012 Your company
+					</div>
+					<div class="span4">
+						<i class="icon-phone"></i> &nbsp; 
+						00123 320 000 123 456
+					</div>
+					<div class="span4">
+						<i class="icon-envelope"></i> &nbsp; 
+						hello@yourcomapny.com
+					</div>
+				</div> <!-- end .row -->		
+			</div> <!-- end .container -->		
+		</div>
+
+	</footer>
+
+	<!-- Javascript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+	<script type="text/javascript" src="js/jquery.touchSwipe.js"></script>
+	<script type="text/javascript" src="js/jquery.hotkeys.min.js"></script>
+	<script type="text/javascript" src="js/functions.min.js?v=2"></script>
+
+</body>
+</html>
